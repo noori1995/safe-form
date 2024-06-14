4cb96131c54454bbefbfbccaac9334c5226e4ceb
noori <mohamad10.noori@gmail.com>
2024-06-14 18:46:04 +0330

    fix

--- back/server.js
+++ back/server.js
@@ -28,6 +28,7 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
+  validate: { xForwardedForHeader: false },
 });
 
 app.use("/api/", limiter);
