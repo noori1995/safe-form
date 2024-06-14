77dfab0e1b7e69c8c18d3d20c300e99da2e1bd9d
noori <mohamad10.noori@gmail.com>
2024-06-14 22:05:34 +0330

    fix

--- back/files/js.js
+++ back/files/js.js
@@ -5,44 +5,36 @@ async function getCsrfToken() {
   const data = await response.json();
   return data.csrfToken;
 }
-  grecaptcha.ready(function () {
-    grecaptcha
-      .execute("6LeuE_kpAAAAAIlv6byBxRDNvO06_UNBu3soMmZr", { action: "submit" })
-      .then(function (token) {
-        console.log('grecaptcha ok')
-        async function handleSubmit(event) {
-          event.preventDefault();
 
-          const csrfToken = await getCsrfToken();
+async function handleSubmit(event) {
+  event.preventDefault();
 
-          const formData = {
-            name: document.getElementById("name").value,
-            email: document.getElementById("email").value,
-            _csrf: csrfToken,
-          };
+  const csrfToken = await getCsrfToken();
 
-          const response = await fetch("/api/submit-form", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "CSRF-Token": csrfToken,
-            },
-            body: JSON.stringify({ ...formData, recaptchaToken }),
-          });
+  const formData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    _csrf: csrfToken,
+  };
 
-          const result = await response.json();
-
-          if (response.ok) {
-            alert(result.message);
-          } else {
-            alert(`Error: ${result.message}`);
-          }
-        }
-
-        document
-          .getElementById("myForm")
-          .addEventListener("submit", handleSubmit);
-
-        // Add your logic to submit to your backend server here.
-      });
+  const response = await fetch("/api/submit-form", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "CSRF-Token": csrfToken,
+    },
+    body: JSON.stringify({ ...formData, recaptchaToken }),
   });
+
+  const result = await response.json();
+
+  if (response.ok) {
+    alert(result.message);
+  } else {
+    alert(`Error: ${result.message}`);
+  }
+}
+
+document.getElementById("myForm").addEventListener("submit", handleSubmit);
+
+// Add your logic to submit to your backend server here.
