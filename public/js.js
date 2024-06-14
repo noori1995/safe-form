7fbc8f0382dc10dd31caa713034c71e14e3b7467
noori <mohamad10.noori@gmail.com>
2024-06-14 21:15:52 +0330

    fix

--- public/js.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  fetch("/form")
-    .then((response) => response.json())
-    .then((data) => {
-      document.getElementById("token").value = data.sessionToken;
-      document.getElementById("csrf").value = data.csrfToken;
-    })
-    .catch((error) => console.error("Error fetching tokens:", error));
-});
